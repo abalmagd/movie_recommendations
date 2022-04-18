fab368afbafc2a8bee19471da266f9177c8e7aa4
Mahmoud Abulmagd <m.abalmagd@gmail.com>
2022-04-18 15:43:53 +0200

    - Add Theme and File structure

---
+++ lib/theme/palette.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const almostBlack = Color(0xFF040b13);
+  static const red100 = Color(0xFFffcdd2);
+  static const red200 = Color(0xFFef9a9a);
+  static const red300 = Color(0xFFe57373);
+  static const red400 = Color(0xFFef5350);
+  static const red500 = Color(0xFFf44336);
+  static const red600 = Color(0xFFe53935);
+  static const red700 = Color(0xFFd32f2f);
+  static const red800 = Color(0xFFc62828);
+  static const red900 = Color(0xFFb71c1c);
+}
