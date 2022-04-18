a4be42bb9b0d2e2bbf0fcfdea0fd019443626d07
Mahmoud Abulmagd <m.abalmagd@gmail.com>
2022-04-18 16:04:19 +0200

    - Add Theme and File structure - Page flow class

---
+++ lib/features/movie_flow/movie_flow.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MovieFlow extends StatefulWidget {
+  const MovieFlow({Key? key}) : super(key: key);
+
+  @override
+  State<MovieFlow> createState() => _MovieFlowState();
+}
+
+class _MovieFlowState extends State<MovieFlow> {
+  late final PageController pageController;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint(pageController.toString());
+    return PageView();
+  }
+}
