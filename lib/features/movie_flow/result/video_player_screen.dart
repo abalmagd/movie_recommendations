423c1624940edf4f4ab2691be0879acdec69e06d
Mahmoud Abulmagd <m.abalmagd@gmail.com>
2022-05-29 08:49:25 +0200

    Testing another youtube player

--- lib/features/movie_flow/result/video_player_screen.dart
+++ lib/features/movie_flow/result/video_player_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoPlayerScreen extends StatefulWidget {
   const VideoPlayerScreen({
@@ -16,28 +15,16 @@ class VideoPlayerScreen extends StatefulWidget {
 }
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-  late final YoutubePlayerController _controller;
 
   void listener() {}
 
   @override
   void initState() {
     super.initState();
-    _controller = YoutubePlayerController(
-      initialVideoId: widget.videoId,
-      flags: const YoutubePlayerFlags(
-        hideThumbnail: true,
-        disableDragSeek: true,
-        // hideControls: true,
-        useHybridComposition: true,
-      ),
-    );
   }
 
   @override
   void dispose() {
-    _controller.removeListener(listener);
-    _controller.dispose();
     super.dispose();
   }
 
@@ -52,35 +39,8 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
           sigmaX: 3.0,
           sigmaY: 3.0,
         ),
-        child: Center(
-          child: YoutubePlayer(
-            controller: _controller,
-            topActions: [
-              BackButton(
-                onPressed: () {
-                  if (_controller.value.isFullScreen) {
-                    _controller.toggleFullScreenMode();
-                  }
-                  Navigator.pop(context);
-                },
-                color: Colors.white,
-              ),
-            ],
-            bottomActions: [
-              const SizedBox(width: 14.0),
-              CurrentPosition(),
-              const SizedBox(width: 8.0),
-              ProgressBar(
-                isExpanded: true,
-                colors: ProgressBarColors(
-                  playedColor: Theme.of(context).colorScheme.primary,
-                  handleColor: Colors.white,
-                ),
-              ),
-              RemainingDuration(),
-            ],
-            showVideoProgressIndicator: true,
-          ),
+        child: const Center(
+          child: Placeholder(),
         ),
       ),
     );
