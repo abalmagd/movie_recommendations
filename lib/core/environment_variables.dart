41f5dd48c94809e1fc77b293661812a65822fa5a
Mahmoud Abulmagd <me@abulmagd.dev>
2022-12-04 09:23:11 +0200

    api key

--- lib/core/environment_variables.dart
+++ lib/core/environment_variables.dart
@@ -1,5 +1,5 @@
-const apiKey = String.fromEnvironment('apiKey');
-const v4AuthKey = String.fromEnvironment('v4AuthKey');
+const apiKey = '3e14189a0b6a6b4eea346e731775cae3';
+const v4AuthKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTE0MTg5YTBiNmE2YjRlZWEzNDZlNzMxNzc1Y2FlMyIsInN1YiI6IjYyNjMwMzE3ZjQ5NWVlMGUxMjk5Nzg0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hjkGjKyFyqvOyHZP_XR0ePyuuG4e-czUD24vSFbuCrs';
 
 const baseUrl = 'https://api.themoviedb.org/3/';
 const genresEndpoint = 'genre/movie/list';
